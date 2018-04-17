494504d0daa9ffabdfd67e897558bd064b49a475
Matt Hamil <j.matthew.hamil@gmail.com>
2018-04-17 01:04:12 -0500

    removed {" "} from EmptyCartMsg comp

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -42,7 +42,7 @@ const EmptyCartMessage = () => (
     <span>
       <span role="img" aria-label="warning">
         ⚠️
-      </span>{" "}
+      </span>
       Your cart is empty.
     </span>
   </div>
